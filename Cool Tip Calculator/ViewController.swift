e87924f6bd9117eec138dfddc99c635f7f92c2c5
Karol Phung <phungkarol@gmail.com>
2020-07-11 12:04:21 -0400

    Initial Commit

---
+++ Cool Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cool Tip Calculator
+//
+//  Created by Karol Phung on 7/11/20.
+//  Copyright © 2020 Karol Phung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
